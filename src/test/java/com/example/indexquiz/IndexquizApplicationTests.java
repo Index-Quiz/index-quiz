2ab761298e36557aab3315a50fc54b9413274b8e
coli <kkwoo001021@naver.com>
2025-07-05 05:44:32 +0900

    chore: 초기 프로젝트 세팅

---
+++ src/test/java/com/example/indexquiz/IndexquizApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.indexquiz;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IndexquizApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
