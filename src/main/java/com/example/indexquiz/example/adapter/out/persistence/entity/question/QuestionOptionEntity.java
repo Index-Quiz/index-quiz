1a612898804f2794d10298a71448198d6aa67314
coli <kkwoo001021@naver.com>
2025-07-29 18:16:06 +0900

    feat: QuestionOptionMapper 구현

--- src/main/java/com/example/indexquiz/example/adapter/out/persistence/entity/question/QuestionOptionEntity.java
+++ src/main/java/com/example/indexquiz/example/adapter/out/persistence/entity/question/QuestionOptionEntity.java
@@ -10,11 +10,15 @@ import jakarta.persistence.Table;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
 import lombok.AccessLevel;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
 import lombok.NoArgsConstructor;
 
 @Entity
+@Getter
 @Table(name = "question_options")
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
+@AllArgsConstructor(access = AccessLevel.PUBLIC)
 public class QuestionOptionEntity {
 
     @Id
