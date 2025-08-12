10c3b2483b4d3b18b880d7f3fef9f937e1f8664b
coli <kkwoo001021@naver.com>
2025-08-12 15:23:36 +0900

    chore: image entity 제거

--- src/main/java/com/example/indexquiz/example/adapter/out/persistence/jparepository/question/QuestionImageJpaRepository.java
+++
@@ -1,8 +0,0 @@
-package com.example.indexquiz.example.adapter.out.persistence.jparepository.question;
-
-import com.example.indexquiz.example.adapter.out.persistence.entity.question.QuestionImageEntity;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface QuestionImageJpaRepository extends JpaRepository<QuestionImageEntity, Long> {
-
-}
--- src/main/java/com/example/indexquiz/example/adapter/out/persistence/jparepository/solution/SolutionImageJpaRepository.java
+++
@@ -1,8 +0,0 @@
-package com.example.indexquiz.example.adapter.out.persistence.jparepository.solution;
-
-import com.example.indexquiz.example.adapter.out.persistence.entity.solution.SolutionImageEntity;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface SolutionImageJpaRepository extends JpaRepository<SolutionImageEntity, Long> {
-
-}
