b8a496643742ac43d94f7f75082a177d54c63044
coli <kkwoo001021@naver.com>
2025-08-29 20:06:04 +0900

    feat: GetAnswerPort 구현

--- src/main/java/com/example/indexquiz/answer/adapter/out/persistence/AnswerJpaRepository.java
+++ src/main/java/com/example/indexquiz/answer/adapter/out/persistence/AnswerJpaRepository.java
@@ -1,7 +1,10 @@
 package com.example.indexquiz.answer.adapter.out.persistence;
 
+import com.example.indexquiz.answer.domain.Answer;
+import java.util.List;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 interface AnswerJpaRepository extends JpaRepository<AnswerEntity, Long> {
 
+    List<AnswerEntity> findAllByQuestionId(long questionId);
 }
--- src/main/java/com/example/indexquiz/answer/domain/Answers.java
+++ src/main/java/com/example/indexquiz/answer/domain/Answers.java
@@ -1,8 +1,10 @@
 package com.example.indexquiz.answer.domain;
 
 import java.util.List;
+import lombok.Getter;
 import lombok.RequiredArgsConstructor;
 
+@Getter
 @RequiredArgsConstructor
 public class Answers {
 
