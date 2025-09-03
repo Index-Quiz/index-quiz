13f247a91794ff38d3987149bc200493a7ee476f
coli <kkwoo001021@naver.com>
2025-09-04 02:37:31 +0900

    test: 실패하는 테스트 수정

--- src/test/java/com/example/indexquiz/BaseControllerTest.java
+++ src/test/java/com/example/indexquiz/BaseControllerTest.java
@@ -5,7 +5,9 @@ import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.web.server.LocalServerPort;
+import org.springframework.test.context.ActiveProfiles;
 
+@ActiveProfiles("test")
 @ExtendWith(DataBaseCleaner.class)
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 public abstract class BaseControllerTest {
