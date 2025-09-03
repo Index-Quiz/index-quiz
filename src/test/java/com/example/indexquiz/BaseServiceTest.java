04dd6e3b5724678b6371ea4cd0e1012ab4b6a96d
coli <kkwoo001021@naver.com>
2025-09-04 02:37:31 +0900

    test: 실패하는 테스트 수정

--- src/test/java/com/example/indexquiz/BaseServiceTest.java
+++ src/test/java/com/example/indexquiz/BaseServiceTest.java
@@ -2,7 +2,9 @@ package com.example.indexquiz;
 
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 
+@ActiveProfiles("test")
 @ExtendWith(DataBaseCleaner.class)
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.NONE)
 public abstract class BaseServiceTest {
