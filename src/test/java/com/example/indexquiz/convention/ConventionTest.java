ffd7b7f32b0e20df7a63bd75eb2dd226b571b533
김건우 <kkwoo001021@naver.com>
2025-09-10 22:40:36 +0900

    [TEST] ArchUnit을 활용한 의존성 테스트 작성 (#32)
    * config: archunit 의존성 추가
    
    * test: 의존성 테스트 작성
    
    * test: 어노테이션 테스트 작성
    
    * test: 네이밍 테스트 작성
    
    * test: Controller 어노테이션 추가
    
    * style: 공백 추가
    
    * chore: 패키지명 변경
    
    dependency > Convention
    
    * chore: 주석 변경

---
+++ src/test/java/com/example/indexquiz/convention/ConventionTest.java
@@ -0,0 +1,227 @@
+package com.example.indexquiz.convention;
+
+import com.tngtech.archunit.core.domain.JavaClasses;
+import com.tngtech.archunit.core.importer.ClassFileImporter;
+import com.tngtech.archunit.core.importer.ImportOption;
+import com.tngtech.archunit.lang.ArchRule;
+import com.tngtech.archunit.lang.syntax.ArchRuleDefinition;
+import com.tngtech.archunit.library.dependencies.SliceRule;
+import com.tngtech.archunit.library.dependencies.SlicesRuleDefinition;
+import jakarta.persistence.Entity;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Disabled;
+import org.junit.jupiter.api.Nested;
+import org.junit.jupiter.api.Test;
+import org.springframework.stereotype.Controller;
+import org.springframework.stereotype.Service;
+import org.springframework.web.bind.annotation.RestController;
+
+@SuppressWarnings("NonAsciiCharacters")
+public class ConventionTest {
+
+    private JavaClasses importedClasses;
+
+    @BeforeEach
+    void setUp() {
+        importedClasses = new ClassFileImporter()
+                .withImportOption(ImportOption.Predefined.DO_NOT_INCLUDE_TESTS)
+                .importPackages("com.example.indexquiz");
+    }
+
+    @Nested
+    class DependencyRuleTest {
+
+        // 규칙1. Adapter는 Port 명세를 구현한다
+        // 규칙2. Application.Service 는 UseCase 명세를 구현한다
+        // 규칙3. Domain <- Application <-Adapter 의존성을 따라야 한다
+        // 3-1) Domain은 Application, Adapter 중 어떤 의존성도 가지면 안된다
+        // 3-2) Application.service는 추상화된 명세(UserCase, Port)하고만 소통한다
+        // 3-3) Application.service는 명세의 구현체(Adapter)를 모른다
+        // 3-4) JpaEntity는 persistence 패키지 내에서만 참조되어야 한다
+        // 3-5) Controller는 UseCase를 통해 도메인과 소통한다
+
+        @Test
+        void 아웃패키지의_어댑터는_아웃포트를_구현하여야_한다() {
+            ArchRule rule = ArchRuleDefinition.classes()
+                    .that()
+                    .resideInAPackage("..out..")
+                    .and()
+                    .haveSimpleNameEndingWith("Adapter")
+                    .should()
+                    .dependOnClassesThat()
+                    .haveSimpleNameEndingWith("Port");
+
+            rule.check(importedClasses);
+        }
+
+        @Test
+        void 어플리케이션_서비스는_하나이상의_유즈케이스를_구현하여야_한다() {
+            ArchRule rule = ArchRuleDefinition.classes()
+                    .that()
+                    .resideInAPackage("..application.service..")
+                    .and()
+                    .areAnnotatedWith(Service.class)
+                    .should()
+                    .dependOnClassesThat()
+                    .haveSimpleNameEndingWith("UseCase");
+
+            rule.check(importedClasses);
+        }
+
+        @Test
+        void 도메인은_바깥계층과의_의존성을_지니면_안된다() {
+            ArchRule rule = ArchRuleDefinition.noClasses()
+                    .that()
+                    .resideInAPackage("..domain..")
+                    .should()
+                    .dependOnClassesThat()
+                    .resideInAnyPackage("..adapter..", "..application..");
+
+            rule.check(importedClasses);
+        }
+
+        @Test
+        void 어플리케이션_서비스는_포트_유즈케이스을_통해_도메인을_소통한다() {
+            ArchRule rule = ArchRuleDefinition.classes()
+                    .that()
+                    .resideInAPackage("..application.service..")
+                    .should()
+                    .onlyHaveDependentClassesThat()
+                    .resideInAnyPackage("..domain..", "..application.port..");
+
+            rule.check(importedClasses);
+        }
+
+        @Test
+        void 어플리케이션_서비스는_소통_명세의_구현체인_어뎁터를_모른다() {
+            ArchRule rule = ArchRuleDefinition.noClasses()
+                    .that()
+                    .resideInAPackage("..application.service..")
+                    .should()
+                    .dependOnClassesThat()
+                    .resideInAnyPackage("..adapter..");
+
+            rule.check(importedClasses);
+        }
+
+        @Test
+        void jpa엔티티는_어댑터_패키지_내에서만_참조되어야_한다() {
+            ArchRule rule = ArchRuleDefinition.classes()
+                    .that()
+                    .areAnnotatedWith(Entity.class)
+                    .should()
+                    .onlyBeAccessed()
+                    .byAnyPackage("..adapter.out..");
+
+            rule.check(importedClasses);
+        }
+
+        @Test
+        void 컨트롤러는_유즈케이스를_통해_도메인과_소통한다() {
+            ArchRule rule = ArchRuleDefinition.classes()
+                    .that()
+                    .areAnnotatedWith(RestController.class)
+                    .should()
+                    .onlyHaveDependentClassesThat()
+                    .resideInAPackage("..port.in..");
+
+            rule.check(importedClasses);
+        }
+    }
+
+    @Nested
+    class NamingRuleTest {
+
+        @Test
+        void 유즈케이스는_UserCase로_네이밍이_끝나야한다() {
+            ArchRule rule = ArchRuleDefinition.classes()
+                    .that()
+                    .resideInAPackage("..port.in..")
+                    .and()
+                    .areInterfaces()
+                    .should()
+                    .haveSimpleNameEndingWith("UseCase");
+
+            rule.check(importedClasses);
+        }
+
+        @Test
+        @Disabled
+        void 포트는_Port로_네이밍이_끝나야한다() {
+            ArchRule rule = ArchRuleDefinition.classes()
+                    .that()
+                    .resideInAPackage("..port.out..")
+                    .and()
+                    .areInterfaces()
+                    .should()
+                    .haveSimpleNameEndingWith("Port");
+
+            rule.check(importedClasses);
+        }
+
+        @Test
+        void 서비스는_Service로_네이밍이_끝나야한다() {
+            ArchRule rule = ArchRuleDefinition.classes()
+                    .that()
+                    .areAnnotatedWith(Service.class)
+                    .should()
+                    .haveSimpleNameEndingWith("Service");
+
+            rule.check(importedClasses);
+        }
+
+        @Test
+        void 컨트롤러는_Controller로_네이밍이_끝나야한다() {
+            ArchRule rule = ArchRuleDefinition.classes()
+                    .that()
+                    .areAnnotatedWith(RestController.class)
+                    .or().areAnnotatedWith(Controller.class)
+                    .should()
+                    .haveSimpleNameEndingWith("Controller");
+
+            rule.check(importedClasses);
+        }
+    }
+
+    @Nested
+    class CycleTest {
+
+        @Test
+        void 순환참조가_존재해서는_안된다() {
+            SliceRule sliceRule = SlicesRuleDefinition.slices()
+                    .matching("com.example.indexquiz.(*)..")
+                    .should()
+                    .beFreeOfCycles();
+
+            sliceRule.check(importedClasses);
+        }
+    }
+
+    @Nested
+    class AnnotationTest {
+
+        @Test
+        void 컨트롤러에는_RestController_어노테이션이_붙어있어야_한다() {
+            ArchRule rule = ArchRuleDefinition.classes()
+                    .that()
+                    .haveSimpleNameEndingWith("Controller")
+                    .should()
+                    .beAnnotatedWith(RestController.class)
+                    .orShould()
+                    .beAnnotatedWith(Controller.class);
+
+            rule.check(importedClasses);
+        }
+
+        @Test
+        void 서비스는_Service_어노테이션이_붙어있어야_한다() {
+            ArchRule rule = ArchRuleDefinition.classes()
+                    .that()
+                    .haveSimpleNameEndingWith("Service")
+                    .should()
+                    .beAnnotatedWith(Service.class);
+
+            rule.check(importedClasses);
+        }
+    }
+}
