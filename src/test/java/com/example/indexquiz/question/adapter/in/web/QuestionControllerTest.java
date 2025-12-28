be11e894b7ebe44b1ac6d72385e3959e7a34c1dd
coli <kkwoo001021@naver.com>
2025-12-29 01:15:36 +0900

    feat: 오답률 best 7 문제집 기능 테스트 코드 작성

--- src/test/java/com/example/indexquiz/question/adapter/in/web/QuestionControllerTest.java
+++ src/test/java/com/example/indexquiz/question/adapter/in/web/QuestionControllerTest.java
@@ -10,6 +10,8 @@ import com.example.indexquiz.BaseControllerTest;
 import com.example.indexquiz.question.application.port.in.QuestionUseCase;
 import com.example.indexquiz.question.application.port.in.dto.GetQuestionOptionResponse;
 import com.example.indexquiz.question.application.port.in.dto.GetQuestionResponse;
+import com.example.indexquiz.question.application.port.in.dto.GetQuestionResponses;
+import com.example.indexquiz.question.domain.QuestionSet;
 import com.example.indexquiz.question.domain.QuestionType;
 import io.restassured.RestAssured;
 import java.util.List;
@@ -47,6 +49,43 @@ class QuestionControllerTest extends BaseControllerTest {
             // then
             assertAll(
                     () -> then(questionUseCase).should().getQuestion(1),
+                    () -> assertThat(actual).isEqualTo(expected)
+            );
+        }
+    }
+
+    @Nested
+    class GetAllQuestion {
+
+        @Test
+        void 다건의_질문을_조회한다() {
+            // given
+            GetQuestionResponses expected = new GetQuestionResponses(List.of(
+                    new GetQuestionResponse(1L, QuestionType.SINGLE_CHOICE, "질문 내용1",
+                    List.of(new GetQuestionOptionResponse(11L, "선택지 내용11"),
+                            new GetQuestionOptionResponse(12L, "선택지 내용12"),
+                            new GetQuestionOptionResponse(13L, "선택지 내용13"))),
+                    new GetQuestionResponse(2L, QuestionType.SINGLE_CHOICE, "질문 내용2",
+                            List.of(new GetQuestionOptionResponse(21L, "선택지 내용21"),
+                                    new GetQuestionOptionResponse(22L, "선택지 내용22"),
+                                    new GetQuestionOptionResponse(23L, "선택지 내용23")))
+            ));
+
+            given(questionUseCase.getAllQuestions(QuestionSet.BEST_DIFFICULT)).willReturn(expected);
+
+            // when
+            GetQuestionResponses actual = RestAssured.given().log().all()
+                    .queryParam("type", "BEST_DIFFICULT")
+                    .when().get("/api/questions")
+                    .then().log().all()
+                    .statusCode(200)
+                    .extract()
+                    .jsonPath()
+                    .getObject("", GetQuestionResponses.class);
+
+            // then
+            assertAll(
+                    () -> then(questionUseCase).should().getAllQuestions(QuestionSet.BEST_DIFFICULT),
                     () -> assertThat(actual).isEqualTo(expected)
             );
         }
